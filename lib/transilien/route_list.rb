5b460eb4db8c79d4bf3968f87f8c2ddddcccdb3b
Thomas Lecavelier <tl@pierlis.com>
2012-10-30 15:12:37 +0100

    Implements RouteList action

---
+++ lib/transilien/route_list.rb
@@ -0,0 +1,11 @@
+class Transilien::RouteList < Transilien::MicroService
+
+  class << self
+
+    def action
+      'RouteList'
+    end
+
+  end
+
+end
--- lib/transilien_microservices.rb
+++ lib/transilien_microservices.rb
@@ -1,6 +1,7 @@
 require "./lib/transilien/version.rb"
 require "./lib/transilien/micro_service.rb"
 require "./lib/transilien/network_list.rb"
+require "./lib/transilien/route_list.rb"
 
 module Transilien
   # Your code goes here...
