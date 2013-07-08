39b800fc58c9cec706a68fd6ec149c3eb287c15b
Thomas Lecavelier <thomas@lecavelier.name>
2013-07-09 01:29:34 +0200

    Complete Mode action specific mapping

--- lib/transilien/mode.rb
+++ lib/transilien/mode.rb
@@ -1,2 +1,18 @@
 class Transilien::Mode < Transilien::MicroService
+
+  attr_accessor :mode_type
+
+  class << self
+
+    def find(filters={})
+      modes = super(filters)
+      modes.each do |mode|
+        mode.mode_type = mode.payload['ModeTypeExternalCode']
+      end
+      modes
+    end
+
+  end
+
+
 end
