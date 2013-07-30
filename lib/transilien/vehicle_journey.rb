7742dd73d0c0c7bba5149350c8ba494bf2ad627e
Thomas Lecavelier <tl@pierlis.com>
2013-07-30 14:04:29 +0200

    WIP VehicleJourney, need to define helper objet Stop to handle the StopPoint StopArea and other datas

--- lib/transilien/vehicle_journey.rb
+++ lib/transilien/vehicle_journey.rb
@@ -1,2 +1,25 @@
 class Transilien::VehicleJourney < Transilien::MicroService
+  def route
+    @route ||= Transilien::Route.from_node(payload.at('Route'), access_time)
+  end
+  def mode
+    @mode ||= Transilien::Mode.from_node(payload.at('Mode'), access_time)
+  end
+  def company
+    @company ||= Transilien::Company.from_node(payload.at('Company'), access_time)
+  end
+  def vehicle
+    @vehicle ||= payload.at('Vehicle')
+  end
+  def validity_pattern
+    @validity_pattern ||= payload.at('ValidityPattern')
+  end
+
+  def stops
+    @stops ||= begin
+      stops_node = payload.at('StopList')
+      new_stops = []
+
+    end
+  end
 end
