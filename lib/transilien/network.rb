69577c076409b00bd982f138cdee5cdf7d1d5eac
Thomas Lecavelier <tl@pierlis.com>
2013-12-23 20:54:42 +0100

    Let Network instance expose their line_codes via Network#codes

--- lib/transilien/network.rb
+++ lib/transilien/network.rb
@@ -2,4 +2,8 @@ class Transilien::Network < Transilien::MicroService
   def lines
     @lines ||= Transilien::Line.find(network_external_code: external_code)
   end
+
+  def codes
+    @codes ||= lines.line_code.uniq.sort
+  end
 end
